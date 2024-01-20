39e03276eb499c2fbb2faca990ffdc99481ec93d
Tyson Cheung <cheungtyson@gmail.com>
2024-01-20 02:15:51 -0500

    reinstall pip and venv

--- flask-server/.venv/Scripts/activate.bat
+++ flask-server/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Tyson\OneDrive\Documents\Career Launch 2024\careerlaunch2024\flask-server\.venv
+set VIRTUAL_ENV=c:\Users\Tyson\OneDrive\Documents\Career Launch 2024\careerlaunch2024\flask-server\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 

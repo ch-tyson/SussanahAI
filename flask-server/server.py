c31cce6cffd74988bb98b434eacf01d85e3b0744
Tyson Cheung <cheungtyson@gmail.com>
2024-01-22 19:08:46 -0500

    code cleanup

--- flask-server/server.py
+++ flask-server/server.py
@@ -5,9 +5,6 @@ from spam_classify import get_spam_result, get_spam_percentage
 from sentiment_analysis import get_sentiment_scores, get_sentiment_result
 from summarize import summarize
 
-# # Initialize the Cohere client with my API key just trying to figure out another way than just placing here (Mohith)
-# co = cohere.Client('apiKey here')
-
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -16,7 +13,7 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 def index():
 	return {'message': 'Hello, my name is SussannahAI!'}
 
-@app.route("/classify", methods=["POST"])
+@app.route("/spam", methods=["POST"])
 def spam():
     options = request.json["options"]
     text = request.json["paragraph"]
@@ -30,28 +27,13 @@ def spam():
             responseData["spam"] = spam
             responseData["spamValue"] = spam_value
         elif option == "summary":
-            summary = summarize(process_text)
+            summary = summarize(processed_text)
             responseData["summary"] = summary
         elif option == "sentiment":
             sentiment = get_sentiment_result(processed_text)
-            sentiment_value = get_sentiment_percentage(processed_text)
+            sentiment_value = get_sentiment_scores(processed_text)
             responseData["sentiment"] = sentiment
             responseData["sentimentValue"] = sentiment_value
-
-    response = jsonify(responseData)
-    
-    return response
-
-@app.route("/classify", methods=["POST"])
-def summarize():
-    options = request.json["options"]
-    text = request.json["paragraph"]
-    processed_text = text
-
-    responseData = None
-    for option in options:
-        if option == "summarize":
-            responseData = summary(processed_text)
 
     response = jsonify(responseData)
     
